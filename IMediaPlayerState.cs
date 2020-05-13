f86429e32310c24eea820f5d69f3b533fe2c280e
Saverio Delpriori <saverio.delpriori@uniurb.it>
2020-05-13 19:46:00 +0200

    Initial commit

---
+++ IMediaPlayerState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatePatternExerciseStub
+{
+    interface IMediaPlayerState
+    {
+        void Play(MediaPlayer ctx);
+        void Pause(MediaPlayer ctx);
+        void Stop(MediaPlayer ctx);
+    }
+}
